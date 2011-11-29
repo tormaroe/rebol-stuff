fb1f1e01a6706c2aa9e7122e08a959580d259a09
tormaroe <torbjorn.maro@gmail.com>
2011-11-29 11:04:03 +0100

    GUI done

--- intouch.r
+++ intouch.r
@@ -1,4 +1,29 @@
 REBOL [ Title: "PSWinCom Intouch client" ]
 
+sender: "REBOL"
 
-alert "hello"
+sendmessage: func[sender receiver message] [
+  
+]
+
+view layout [
+  backcolor white
+  style label text 50 bold
+  h1 "PSWinCom Intouch"
+  across
+    label "From:"
+    from: field :sender
+  below across
+    label "To:"
+    to: field ""
+  below
+    message: area 258x70
+  below across
+    button "Send" 125 [ 
+      sendmessage from/text to/text message/text
+      message/text: copy "" 
+      show message      
+    ]
+    button "Close" 125 red [ quit ]
+]
+
